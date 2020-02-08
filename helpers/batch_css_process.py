9baaa18d15e644e682a9fc0fec9a5e81b3eb3caa
ferferga <ferferga.fer@gmail.com>
2020-02-08 16:35:00 +0100

    Removed .woff files

--- helpers/batch_css_process.py
+++ helpers/batch_css_process.py
@@ -1,18 +1,43 @@
 newlines = []
 
-with open("font-faces.css", "r") as f:
-    lines = f.readlines()
+def addwoff(path):
+    with open(path, "r") as f:
+        lines = f.readlines()
 
-for key, line in enumerate(lines):
-    if "url(fonts/notosans/woff2/" in line:
-        if ("woff2" in line):
-            filepath = line.split("url(fonts/notosans/woff2/")
-            name = filepath[1]
-            name = name.replace("woff2", "woff")
-            name = name.replace("\n", "")
-            line = line.replace(";", ",\n    url(fonts/notosans/woff/" + name)
-    newlines.append(line)
+    for line in lines:
+        if "url(fonts/notosans/woff2/" in line:
+            if ("woff2" in line):
+                filepath = line.split("url(fonts/notosans/woff2/")
+                name = filepath[1]
+                name = name.replace("woff2", "woff")
+                name = name.replace("\n", "")
+                line = line.replace(";", ",\n    url(fonts/notosans/woff/" + name)
+        newlines.append(line)
 
-with open("font-faces.css","w") as f:
-    for line in newlines:
-        f.write(line)
+    with open(path,"w") as f:
+        for line in newlines:
+            f.write(line)
+
+def removewoff(path):
+    with open(path, "r") as f:
+        lines = f.readlines()
+
+    for line in lines:
+        if "format('woff2')," in line:
+            line = line.replace(",", ";")
+        if "format('woff')" in line:
+            continue
+        newlines.append(line)
+
+    with open(path,"w") as f:
+        for line in newlines:
+            f.write(line)
+
+while True:
+    try:
+        path = input("Type the path of the file to process: ")
+        newlines.clear()
+        removewoff(path)
+        newlines.clear()
+    except KeyboardInterrupt:
+        break
